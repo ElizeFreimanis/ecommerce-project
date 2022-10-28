22ea39281470711b17f172dcc81d2c672ba48a87
Elize Freimanis <elizefreimanis@hotmail.com>
2022-10-28 10:11:31 +0200

    added shopping cart

--- src/components/ShoppingCart/index.js
+++ src/components/ShoppingCart/index.js
@@ -1,7 +1,52 @@
 import * as S from "./styled";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 export default function ShoppingCart() {
+    const [shoppingCartOpen, setShoppingCartOpen] = useState(false);
+    const location = useLocation();
+
+    useEffect(() => {
+        setShoppingCartOpen(false);
+    }, [location]);
+
+    const toggle = (e) => {
+        e.stopPropagation();
+        setShoppingCartOpen(!shoppingCartOpen);
+    };
+
+    useEffect(() => {
+        if (shoppingCartOpen) {
+            const onClick = (e) => {
+                if (!document.getElementById("shoppingCart").contains(e.target)) {
+                    setShoppingCartOpen(false);
+                }
+            };
+
+            window.addEventListener("click", onClick);
+
+            return () => {
+                window.removeEventListener("click", onClick);
+            };
+        }
+    }, [shoppingCartOpen]);
+
     //använd reduce funktionen till att lägga ihop alla priser (?)
 
-    return <S.Icon />;
+    return (
+        <div>
+            <S.Icon onClick={toggle} />
+            <S.Container id='shoppingCart' open={shoppingCartOpen}>
+                <h2 style={{ textAlign: "left" }}>Jag är en produkt</h2>
+                <h2 style={{ textAlign: "left" }}>Jag är en produkt</h2>
+                <h2 style={{ textAlign: "left" }}>Jag är en produkt</h2>
+                <h2 style={{ textAlign: "left" }}>Jag är en produkt</h2>
+                <S.BottomSection>
+                    <S.Total>Total: 350 kr</S.Total>
+                    <S.CheckoutButton>checkout</S.CheckoutButton>
+                </S.BottomSection>
+            </S.Container>
+        </div>
+    );
 }
--- src/components/ShoppingCart/styled.js
+++ src/components/ShoppingCart/styled.js
@@ -1,11 +1,46 @@
 import styled from "styled-components";
 import { FiShoppingBag } from "react-icons/fi";
 import colors from "../../constants/colors";
+import Button from "../Button/Button";
+import fonts from "../../constants/fonts";
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    width: 80%;
+    position: fixed;
+    right: 0;
+    background-color: ${colors.white};
+    height: 100vh;
+    top: 0;
+    padding: 10px;
+    box-shadow: ${(props) => (props.open ? "1px 0px 300px black" : 0)};
+    transform: ${(props) => (props.open ? "translateX(0)" : "translateX(100%)")};
+    transition: transform 0.35s;
+    font-family: ${fonts.firaSans};
+`;
 
 export const Icon = styled(FiShoppingBag)`
     font-size: 27px;
     color: ${colors.white};
     order: 4;
+`;
+
+export const BottomSection = styled.div`
+    position: absolute;
+    bottom: 5%;
+    left: 50%;
+    transform: translateX(-50%);
+`;
+
+export const Total = styled.h3`
+    font-size: 20px;
+`;
+
+export const CheckoutButton = styled(Button)`
+    background-color: ${colors.lightBrown};
+    color: ${colors.white};
+    width: 240px;
+    height: 50px;
+    font-size: 20px;
+    text-transform: uppercase;
+    margin-top: 10px;
 `;
